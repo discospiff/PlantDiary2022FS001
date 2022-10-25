a0ad38b397903aa003dd9db6827e82a83036a590
Brandan <brandan.jones@uc.edu>
2022-10-25 19:57:44 -0400

    Read our first JSON Stream

--- PlantDiary2022FS001/Pages/Index.cshtml.cs
+++ PlantDiary2022FS001/Pages/Index.cshtml.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PlantPlacesSpecimens;
 
 namespace PlantDiary2022FS001.Pages
 {
     public class IndexModel : PageModel
     {
+        static readonly HttpClient client = new HttpClient();
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -21,6 +24,14 @@ namespace PlantDiary2022FS001.Pages
                 brand = inBrand;
             }
             ViewData["Brand"] = brand;
+
+            var task = client.GetAsync("http://plantplaces.com/perl/mobile/specimenlocations.pl?Lat=39.14455&Lng=-84.50939&Range=0.5&Source=location");
+            HttpResponseMessage result = task.Result; 
+            result.EnsureSuccessStatusCode();
+            Task<string> readString = result.Content.ReadAsStringAsync();
+            string jsonString = readString.Result;
+            List<Specimen> specimens = Specimen.FromJson(jsonString);
+            ViewData["Specimens"] = specimens;
         }
     }
 }
