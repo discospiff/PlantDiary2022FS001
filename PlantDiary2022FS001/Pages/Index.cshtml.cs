f4e2acf5401cc8d1d5d8f3e4d5d7b990e448d85a
Brandan <brandan.jones@uc.edu>
2022-10-25 21:47:13 -0400

    Add async/await

--- PlantDiary2022FS001/Pages/Index.cshtml.cs
+++ PlantDiary2022FS001/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using PlantPlacesPlants;
 using PlantPlacesSpecimens;
 using PlantPlacesWeather;
+using System.Xml.Serialization;
 
 namespace PlantDiary2022FS001.Pages
 {
@@ -27,65 +28,79 @@ namespace PlantDiary2022FS001.Pages
             }
             ViewData["Brand"] = brand;
 
-            GetData();
+            Task<List<Specimen>> task = GetData();
+            List<Specimen> waterLovingSpecimens = task.Result;
+            ViewData["Specimens"] = waterLovingSpecimens;
 
         }
 
-        private async void GetData()
+        private async Task<List<Specimen>> GetData()
         {
-            // get plant specimens at this location.
-            var task = client.GetAsync("http://plantplaces.com/perl/mobile/specimenlocations.pl?Lat=39.14455&Lng=-84.50939&Range=0.5&Source=location");
-            HttpResponseMessage result = task.Result;
-            result.EnsureSuccessStatusCode();
-            Task<string> readString = result.Content.ReadAsStringAsync();
-            string jsonString = readString.Result;
-            List<Specimen> specimens = Specimen.FromJson(jsonString);
-
-
-            // get the data for water loving plants.
-            Task<HttpResponseMessage> plantTask = client.GetAsync("http://plantplaces.com/perl/mobile/viewplantsjsonarray.pl?WetTolerant=on");
-            HttpResponseMessage plantResult = plantTask.Result;
-            Task<string> plantTaskString = plantResult.Content.ReadAsStringAsync();
-            string plantJson = plantTaskString.Result;
-            List<Plant> plants = Plant.FromJson(plantJson);
-
-            // Combine our data together.
-            IDictionary<long, Plant> waterLovingPlants = new Dictionary<long, Plant>();
-            foreach (Plant plant in plants)
+            return await Task.Run(async () =>
             {
-                waterLovingPlants[plant.Id] = plant;
-            }
-            List<Specimen> waterLovingSpecimens = new List<Specimen>();
-            foreach (Specimen specimen in specimens)
-            {
-                if (waterLovingPlants.ContainsKey(specimen.PlantId))
+                // get plant specimens at this location.
+                var task = client.GetAsync("http://plantplaces.com/perl/mobile/specimenlocations.pl?Lat=39.14455&Lng=-84.50939&Range=0.5&Source=location");
+                HttpResponseMessage result = task.Result;
+                result.EnsureSuccessStatusCode();
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString = readString.Result;
+                List<Specimen> specimens = Specimen.FromJson(jsonString);
+
+
+                // get the data for water loving plants.
+                Task<HttpResponseMessage> plantTask = client.GetAsync("http://plantplaces.com/perl/mobile/viewplantsjsonarray.pl?WetTolerant=on");
+                HttpResponseMessage plantResult = plantTask.Result;
+                Task<string> plantTaskString = plantResult.Content.ReadAsStringAsync();
+                string plantJson = plantTaskString.Result;
+                List<Plant> plants = Plant.FromJson(plantJson);
+
+                // Combine our data together.
+                IDictionary<long, Plant> waterLovingPlants = new Dictionary<long, Plant>();
+                foreach (Plant plant in plants)
                 {
-                    // if we're here, we have a specimen that likes water
-                    waterLovingSpecimens.Add(specimen);
+                    waterLovingPlants[plant.Id] = plant;
                 }
-            }
-            ViewData["Specimens"] = waterLovingSpecimens;
+                List<Specimen> waterLovingSpecimens = new List<Specimen>();
+                foreach (Specimen specimen in specimens)
+                {
+                    if (waterLovingPlants.ContainsKey(specimen.PlantId))
+                    {
+                        // if we're here, we have a specimen that likes water
+                        waterLovingSpecimens.Add(specimen);
+                    }
+                }
+                
 
-            // read in weather data for our locale.
-            String weatherEndpoint = "https://api.weatherbit.io/v2.0/current?&city=Cincinnati&country=USA&key=";
-            Task<HttpResponseMessage> weatherTask = client.GetAsync(weatherEndpoint);
-            HttpResponseMessage weatherResult = await weatherTask;
-            Task<string> weatherTaskString = weatherResult.Content.ReadAsStringAsync();
-            string weatherJson = weatherTaskString.Result;
+                // grab our API key from THE SECRET STORE
+                var config = new ConfigurationBuilder()
+                    .AddUserSecrets<Program>()
+                    .Build();
 
-            double precip = 0;
-            Weather weather = Weather.FromJson(weatherJson);
-            foreach (Datum weatherDatum in weather.Data)
-            {
-                precip = weatherDatum.Precip;
-            }
-            if (precip < 0.5)
-            {
-                ViewData["Message"] = "It's dry!  Water plants.";
-            } else
-            {
-                ViewData["Message"] = "Rain expected.  No need to water.";
-            }
+                string apiKey = config["weatherapikey"];
+
+                // read in weather data for our locale.
+                String weatherEndpoint = "https://api.weatherbit.io/v2.0/current?&city=Cincinnati&country=USA&key=" + apiKey;
+                Task<HttpResponseMessage> weatherTask = client.GetAsync(weatherEndpoint);
+                HttpResponseMessage weatherResult = await weatherTask;
+                Task<string> weatherTaskString = weatherResult.Content.ReadAsStringAsync();
+                string weatherJson = weatherTaskString.Result;
+
+                double precip = 0;
+                Weather weather = Weather.FromJson(weatherJson);
+                foreach (Datum weatherDatum in weather.Data)
+                {
+                    precip = weatherDatum.Precip;
+                }
+                if (precip < 0.5)
+                {
+                    ViewData["Message"] = "It's dry!  Water plants.";
+                }
+                else
+                {
+                    ViewData["Message"] = "Rain expected.  No need to water.";
+                }
+                return waterLovingSpecimens;
+            });
         }
     }
 }
