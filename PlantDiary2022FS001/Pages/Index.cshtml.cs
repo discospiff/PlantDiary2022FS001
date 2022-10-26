43bb67dc1e818204ff0ea7329103a2d345bb777b
Brandan <brandan.jones@uc.edu>
2022-10-25 20:47:03 -0400

    Consume a second data source; use it to filter the first data source and create a subset of water loving plants.

--- PlantDiary2022FS001/Pages/Index.cshtml.cs
+++ PlantDiary2022FS001/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PlantPlacesPlants;
 using PlantPlacesSpecimens;
 
 namespace PlantDiary2022FS001.Pages
@@ -19,19 +20,50 @@ namespace PlantDiary2022FS001.Pages
         {
             string inBrand = Request.Query["Brand"];
             string brand = "My Plant Diary";
-            if (inBrand != null && inBrand.Length >0)
+            if (inBrand != null && inBrand.Length > 0)
             {
                 brand = inBrand;
             }
             ViewData["Brand"] = brand;
 
+            GetData();
+
+        }
+
+        private void GetData()
+        {
+            // get plant specimens at this location.
             var task = client.GetAsync("http://plantplaces.com/perl/mobile/specimenlocations.pl?Lat=39.14455&Lng=-84.50939&Range=0.5&Source=location");
-            HttpResponseMessage result = task.Result; 
+            HttpResponseMessage result = task.Result;
             result.EnsureSuccessStatusCode();
             Task<string> readString = result.Content.ReadAsStringAsync();
             string jsonString = readString.Result;
             List<Specimen> specimens = Specimen.FromJson(jsonString);
-            ViewData["Specimens"] = specimens;
+
+
+            // get the data for water loving plants.
+            Task<HttpResponseMessage> plantTask = client.GetAsync("http://plantplaces.com/perl/mobile/viewplantsjsonarray.pl?WetTolerant=on");
+            HttpResponseMessage plantResult = plantTask.Result;
+            Task<string> plantTaskString = plantResult.Content.ReadAsStringAsync();
+            string plantJson = plantTaskString.Result;
+            List<Plant> plants = Plant.FromJson(plantJson);
+
+            // Combine our data together.
+            IDictionary<long, Plant> waterLovingPlants = new Dictionary<long, Plant>();
+            foreach (Plant plant in plants)
+            {
+                waterLovingPlants[plant.Id] = plant;
+            }
+            List<Specimen> waterLovingSpecimens = new List<Specimen>();
+            foreach (Specimen specimen in specimens)
+            {
+                if (waterLovingPlants.ContainsKey(specimen.PlantId))
+                {
+                    // if we're here, we have a specimen that likes water
+                    waterLovingSpecimens.Add(specimen);
+                }
+            }
+            ViewData["Specimens"] = waterLovingSpecimens;
         }
     }
 }
